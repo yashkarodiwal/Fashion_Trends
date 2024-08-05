58e566e9eced38ec5b53c98f3eb0cf95845bb778
yash karodiwal <yashkarodiwal03@gmail.com>
2024-08-05 12:01:48 +0530

    project started

---
+++ backend/controllers/rootcontroller.js
@@ -0,0 +1,4 @@
+const rootcontroller = (req, res) => {
+    res.send("welcome to fashion trends")
+}
+module.exports = rootcontroller
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const morgan = require("morgan")
+const rootroute = require("./routes/rootroute")
+const PORT = process.env.PORT || 6000 || 8080
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("api is working")
+})
+
+app.use("/fashiontrends", rootroute)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`.bgBlue.white)
+})
